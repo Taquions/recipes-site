c5a49012f86e93663b697ba24d8559993b15a463
Sávio <saviolr7@gmail.com>
2024-02-01 07:25:10 -0300

    update the 404, change the recipe name and correct the html and name of detail recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 '''Module for the views of the recipes app.'''
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
 
@@ -20,24 +19,27 @@ def category(request, category_id):
     '''Fuction to View for the home page.
     Remember to add the app to the list of installed apps
     in projeto/settings.py.'''
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        # category__id is the way to access the foreign key field.
-        is_published=True
-        ).order_by('-id')
-
-    if not recipes:
-        raise Http404('Category not found')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            # category__id is the way to access the foreign key field.
+            is_published=True
+        ).order_by('-id'),
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f"{recipes.first().category.name} | "
+        'title': f"{recipes[0].category.name} | "
     })
 
 
 def recipe(request, recipe_id):
     '''View for the recipe page.'''
-    recipe_ = Recipe.objects.get(id=recipe_id)
+    recipe_ = get_object_or_404(
+        Recipe,
+        id=recipe_id,
+        is_published=True
+        )
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe_,
         'is_detail_page': True,
